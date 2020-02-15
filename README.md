92ecd37fb433d05e662380abf7ece821d2f9a3c3
luofei614 <upfy@qq.com>
2020-02-15 19:13:00 +0800

    sourcemap

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sourcemap
+
+TODO : 封装为node的命令行方便使用
---
+++ sourcemap.js
@@ -0,0 +1,61 @@
+/**
+ * node parse_error.js line column // 参数为行号列号
+ */
+var fs = require('fs');
+var sourceMap = require('source-map');
+var axios = require('axios');
+var mapsourceCache={};
+async function parseError(errorline){
+   let r=errorline.match(/http(.*?)\.js/);
+    if(r){
+        //有发现js文件
+        let jsfile=r[0];
+        let numberarr=errorline.split(":")
+        let aColumn=numberarr.pop();
+        let aLine=numberarr.pop();
+        //请求map文件
+        let res = await axios.get(jsfile+".map");
+        var smc;
+        if(mapsourceCache[jsfile]){
+            smc=mapsourceCache[jsfile];
+        }else{
+            try {
+                smc = await new sourceMap.SourceMapConsumer(res.data)
+                mapsourceCache[jsfile]=smc;
+            } catch (e) {
+                console.log(errorline);
+                return ;
+            }
+        }
+        let parseData = smc.originalPositionFor({
+            line: parseInt(aLine),
+            column: parseInt(aColumn)
+        });
+        if(parseData.source){
+            console.log(errorline+" 👉  "+parseData.source+":"+parseData.line+":"+parseData.column);
+        }else{
+            console.log(errorline);
+        }
+    }else{
+        console.log(errorline);//没有js则直接输出报错
+    }
+}
+async function parse(errormsg){
+
+    console.log("🌈 解析结果：");
+    errorlines=errormsg.split("\n")
+    for(let i in errorlines){
+       await parseError(errorlines[i]);
+    }
+
+}
+
+
+var errormsg = process.argv[2];
+if(!errormsg){
+    console.log("请传参报错信息");
+    return ;
+}
+
+parse(errormsg)
+
